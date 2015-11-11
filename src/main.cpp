9ff19095fb5c937eab58ce6d8844257b35f1cfe8
Agam Agarwal <agammaster@gmail.com>
2015-11-11 19:25:25 +0530

    Test for multiple threads

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,84 @@
 #include <string>
+#include <cstring>
+#include <iostream>
+#include <thread>
+
 #include "WFEHashMap.hpp"
 
 using namespace std;
 
-int main() {
-	WFEHashMap x(5);
+#define NUM_PUTTERS 10
+#define NUM_GETTERS 10
+#define NUM_REMOVERS 10
+#define ITER 10
+
+void putter(WFEHashMap *hm, int me) {
 	string a = "abcde";
 	char *b = new char[6];
 	strcpy(b, a.c_str());
 
-	x.put(b, 5);
-	x.put(b, 10);
+	for (int i = 0; i < ITER; i++) {
+		hm->put(b, 9);
+	}
+}
+
+void getter(WFEHashMap *hm, int me) {
+	string a = "abcde";
+	char *b = new char[6];
+	strcpy(b, a.c_str());
+
+	for (int i = 0; i < ITER; i++) {
+		int* r;
+		r = hm->get(b);
+		if (r == nullptr)
+			cout<<-1<<endl;
+		else
+			cout<<(*r)<<endl;
+	}
+}
+
+void remover(WFEHashMap *hm, int me) {
+	string a = "abcde";
+	char *b = new char[6];
+	strcpy(b, a.c_str());
+
+	for (int i = 0; i < ITER; i++) {
+		cout<<"Remove status: "<<hm->remove(b)<<endl;
+	}
+}
+
+int main() {
+	WFEHashMap *x = new WFEHashMap(5);
+
+
+	thread* putters[NUM_PUTTERS];
+
+	for (int i = 0; i < NUM_PUTTERS; i++) {
+		putters[i] = new thread(putter, x, i);
+	}
+
+	thread* getters[NUM_GETTERS];
+
+	for (int i = 0; i < NUM_GETTERS; i++) {
+		getters[i] = new thread(getter, x, i);
+	}
+
+	thread* removers[NUM_REMOVERS];
+
+	for (int i = 0; i < NUM_REMOVERS; i++) {
+		removers[i] = new thread(remover, x, i);
+	}
+
+	for (int i = 0; i < NUM_PUTTERS; i++) {
+		putters[i]->join();
+	}
+
+	for (int i = 0; i < NUM_GETTERS; i++) {
+		getters[i]->join();
+	}
+
+	for (int i = 0; i < NUM_REMOVERS; i++) {
+		removers[i]->join();
+	}
 	return 0;
 }
